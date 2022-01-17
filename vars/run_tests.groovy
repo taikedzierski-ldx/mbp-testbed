edc6bfab0d9e7f4d1226fe3e78766d96e9d52ecf
Tai Kedzierski <tai.kedzierski@lumiradx.com>
2022-01-17 11:07:21 +0000

    re-error

--- vars/run_tests.groovy
+++ vars/run_tests.groovy
@@ -6,16 +6,23 @@ def call(Map opts) {
     run_sequence = node(label: target) {
         sequence.each { seq ->
             stage("Run $seq") {
+                def success = false
                 catchError(stageResult: "FAILURE") {
                     def result_file = "results_${seq}.txt"
                     echo "Running: $seq"
                     sh "(echo \$HOSTNAME ; env)| tee $result_file"
                     sh "false"
+
+                    success = true
                 }
 
                 println "Stashing ..."
                 stash name: name, includes: "results_*.txt"
                 println "Stashed."
+
+                if(! success) {
+                    sh "false" // this one is not caught
+                }
             }
         }
     }
